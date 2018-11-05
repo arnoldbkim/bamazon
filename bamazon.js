9d8a7f5af8f154c9d247697da50a798e6da484df
Arnold <arnoldbkim@gmail.com>
2018-11-05 13:24:44 -0800

    update

--- bamazon.js
+++ bamazon.js
@@ -45,30 +45,21 @@ function promptUserPurchase() {
 			name: 'quantity',
 			message: 'How many do you need?',
 			filter: Number,
-			validate: function(value) {
-                if (isNaN(value) === false) {
-                return true;
-                }
-                return false;
-            }
+			
 		}
 	]).then(function(answer) {
-		var query = "SELECT product_name, price, stock_quantity FROM products WHERE ?";
-		connection.query(query, { id: answer.chooseProduct }, function(err, res) {
-			for (var i = 0; i < res.length; i++) {
-			console.log("You chose: " + res[i].product_name + " @ Price:$ " + res[i].price + "." + " We have " + res[i].stock_quantity + " in stock.");
-
-
-
-			if (res[i].stock_quantity >= answer.chooseQuantity){
-				console.log("Your total is: " + "$" + (answer.chooseQuantity * res[i].price) + "." + " Thank you for shopping at Bamazon!");
-				} 
-			else if (res[i].stock_quantity <= answer.chooseQuantity){
-                    console.log("Sorry there are only " + res[i].stock_quantity + " in stock! Please choose a different quantity.");
-                    productChoice();
-					};
-			};
-		});
+		var query = 'SELECT product_name, price, stock_quantity FROM products WHERE ?' + answer.quantity;
+        connection.query(query, function(err, res) {
+          if (answer.quantity <= res) {
+            for (var i = 0; i < res.length; i++) {
+                console.log("We currently have " + res[i].stock_quantity + " " + res[i].product_name + ".");
+                console.log("Thank you for your patronage! Your order of "+ res[i].stock_quantity + " " + res[i].product_name + " is now being processed.");
+              }
+            } else {
+              console.log("Not enough of this product in stock.");
+            }
+           
+        });
 	});	
 
 };
